56824cb00a9856a78d3b05e71dcfefe0d380e4a0
tradingpatiently <tradingpatientlyllc@gmail.com>
2026-06-18 16:20:20 -0400

    Add files via upload

---
+++ api/debug.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  return res.status(200).json({
+    hasOpenAI: !!process.env.OPENAI_API_KEY,
+    hasAnthropic: !!process.env.ANTHROPIC_API_KEY,
+    openAILength: process.env.OPENAI_API_KEY ? process.env.OPENAI_API_KEY.length : 0,
+    openAIPrefix: process.env.OPENAI_API_KEY ? process.env.OPENAI_API_KEY.slice(0, 7) : 'missing',
+    nodeEnv: process.env.NODE_ENV,
+  });
+}
